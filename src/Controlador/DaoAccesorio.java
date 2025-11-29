9380728850fa3a1a5260caa503d6200f64309935
lizatamara <lizaa1609@gmail.com>
2025-11-29 01:21:40 -0300

    Add method to insert elements in DAO

--- src/Controlador/DaoAccesorio.java
+++ src/Controlador/DaoAccesorio.java
@@ -3,7 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controlador;
+import BD.Conexion;
+import Modelo.Accesorio;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.CallableStatement; 
+import java.sql.Date;
 /**
  *
  * @author liza_
@@ -15,5 +20,31 @@ public class DaoAccesorio {
     public DaoAccesorio() {
     }
     
+    // agregar
     
+    public boolean agregarAccesorio(Accesorio a) throws SQLException{
+        boolean centinela = false;
+        try {
+            this.conexion = new Conexion().obtenerConexion();
+            String query = "insert into ACCESORIO values(?,?,?,?,?,?,?)";
+            CallableStatement cs = this.conexion.prepareCall(query);
+            cs.setInt(1, a.getId_accesorio());
+            cs.setString(2, a.getTipo());
+            cs.setString(3, a.getMarca());
+            cs.setString(4, a.getModelo());
+            cs.setString(5, String.valueOf(a.getTemporada()));
+            cs.setInt(6, a.getPrecio());
+            if (a.getFecha_ingreso() != null) {
+                cs.setDate(7, new Date(a.getFecha_ingreso().getTime()));
+            } else {
+                cs.setDate(7, new Date(System.currentTimeMillis()));
+            }
+            if(cs.executeUpdate()>0){centinela=true;}
+        } catch (Exception e) {
+            System.err.println("Error add "+e.getMessage());
+        } finally {
+            this.conexion.close();
+        }
+        return centinela;
+    }
 }
